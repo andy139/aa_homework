658a5f23141b41544ffbdbe9e8111515c74c9a45
andy139 <antr900@gmail.com>
2020-01-03 08:00:11 -0800

    Finished stack method

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -1,22 +1,31 @@
 # Implementing stack
 
 class Stack
+
+    
     def initialize
-      # create ivar to store stack here!
+      @stack_array = Array.new
     end
 
     def push(el)
-      # adds an element to the stack
+      @stack_array.push(el)
     end
 
     def pop
-      # removes one element from the stack
+      @stack_array.pop
     end
 
     def peek
-      # returns, but doesn't remove, the top element in the stack
+        @stack_array.last
     end
+
   end
 
 
-  
+ 
+
+
+
+
+
+
