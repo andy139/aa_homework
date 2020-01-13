ce4a589b7b2f9e79fa11c618c447cf7df4f0898a
andy139 <antr900@gmail.com>
2020-01-13 11:37:16 -0800

    Added homework 25D1

---
+++ W5D1/lru_cache.rb
@@ -0,0 +1,34 @@
+class LRUCache
+
+    def initialize(size)
+        @size = size
+        @cache = Array.new
+    end
+
+    def count
+        @cache.length
+    end
+
+    def add(el)
+        if @cache.include?(el)
+            @cache.delete(el)
+            @cache << el
+        elsif count > @size
+            @cache.unshift
+            @cache << el
+        else
+            @cache << el
+
+        end
+    end
+
+    def show
+        p @cache
+    end
+
+    private
+    # helper methods go here!
+
+
+
+end
